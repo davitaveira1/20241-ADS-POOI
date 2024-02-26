309143a9bd609ef265d5ad49221e8eddc18a0a42
Davi Taveira <davitaveira@gmail.com>
2024-02-26 20:10:54 -0300

    aula-01

---
+++ 20241-POOI-producao-Aula01/src/Aula01/Ex01.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula01;
+
+/**
+ *
+ * @author Davi
+ */
+public class Ex01 {
+    
+    public static void main(String[] args) {
+        System.out.println("Seja bem-vindo!");
+    }
+    
+}
