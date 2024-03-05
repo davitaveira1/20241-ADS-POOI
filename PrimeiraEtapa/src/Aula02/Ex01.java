2e53075a52464f073e5d04ce7aa4f21a0a346448
Davi Taveira <davitaveira@gmail.com>
2024-03-05 12:41:37 -0300

    Primeira etapa - Java

---
+++ PrimeiraEtapa/src/Aula02/Ex01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula02;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void main(String[] args) {        
+        
+        Scanner leia = new Scanner(System.in);
+        //entrada
+        System.out.print("Informe o nome: ");
+        String nome = leia.next();
+        
+        //saída
+        System.out.println("O nome digitado foi: "+nome);
+        
+    }
+
+}
---
+++ PrimeiraEtapa/src/Aula02/Ex02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula02;
+
+import java.util.Scanner;
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        //entrada
+        System.out.print("Informe o numero: ");
+        int num = leia.nextInt();
+        
+        //saída
+        System.out.println(num);
+    }
+
+}
