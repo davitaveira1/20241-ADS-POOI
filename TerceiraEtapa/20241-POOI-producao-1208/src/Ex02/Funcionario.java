e6647ae47435d4a842c1794d293796274e478513
Davi Taveira <davitaveira@gmail.com>
2024-08-19 20:02:39 -0300

    aula-1908

---
+++ TerceiraEtapa/20241-POOI-producao-1208/src/Ex02/Funcionario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ex02;
+
+import Ex01.Pessoa;
+
+/**
+ *
+ * @author Davi
+ */
+public class Funcionario extends Pessoa{
+    
+    String matricula;
+    
+    public void exibirDadosFuncionario(){
+        System.out.println("Endereco: "+endereco);
+        
+        
+    }
+    
+}
---
+++ TerceiraEtapa/20241-POOI-producao-1908/src/Ex01/Pessoa.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ex01;
+
+/**
+ *
+ * @author Davi
+ */
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    
+            
+    
+}
